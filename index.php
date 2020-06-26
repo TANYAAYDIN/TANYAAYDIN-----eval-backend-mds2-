a508c98ff27fa18fc389decade21dfd7a1779926
TANYAAYDIN <62376039+TANYAAYDIN@users.noreply.github.com>
2020-06-26 11:20:42 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulaire</title>
+</head>
+<body>
+	<form action="index.php" method="post">
+ <p>Votre nom : <input type="text" name="nom" /></p>
+ <p>Votre âge : <input type="text" name="age" /></p>
+ <p>Votre Mail : <input type="text" name="Mail" /></p>
+ <p>Votre Ville : <input type="text" name="Ville" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+	
+</body>
+</html>
+
+
+
